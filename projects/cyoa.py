b79fb97aa4e7099f2de1520f2617e0940b436733
Jose Guevara <jguev1@live.unc.edu>
2021-09-23 13:52:26 -0400

    PJ00 cyoa

---
+++ projects/cyoa.py
@@ -0,0 +1,146 @@
+"""Prisoner's Dilemma."""
+
+__author__ = "730328302"
+
+from random import randint
+player: str = ""
+points: int = 1
+points_opponent: int = 1
+i: int = 0
+round = int
+COWBOY: str = "\U0001F920"
+SAD_FACE: str = "\U0001F622"
+PRISONER: str = "\U000026D3"
+QUESTION_MARK: str = "\U0001F914"
+BRAIN: str = "\U0001F9E0"
+
+def greet() -> None:
+    global player
+    player = input("Hello there! What's your name? ")
+    print(f"Hi, {player}! Welcome to Prisoner's Dilemma! {PRISONER} {QUESTION_MARK}" )
+    has_heard: str = str(input(f"Have you ever heard of the Prisoner's Dilemma, {player}? Type 'yes' or 'no' "))
+    if has_heard == "yes":
+        print(f"Wow smarty {BRAIN}! We'll be doing a simulation of it today")
+    else:
+        print("It's basically a thought experiment used in Game Theory that shows how two rational actors may make decisions that do not produce the optimal outcome for either actor.")
+        print("We'll be doing a simulation of it today!")
+
+    
+
+def instructions() -> None:
+    print("You and your opponent, Dave, are each given a coin and put on opposite sides of a magic box.")
+    print("When you put a coin in the box, the OTHER player recieves 3 coins.")
+    print("So if both of you put a coin in the box, you each leave with a net 2 coins.")
+    print("However, both of you have to option to choose NOT to put a coin in the box, unbeknownst to your oppponent.")
+    print("if you choose not to put a coin in the box, and your opponent chooses to contribute, you get to keep your coin, and the 3 coins you get from your opponent's contribution.")
+    print("They, on the other hand, gave up one of their coins, and gained no coins from you, so they net -1 coins")
+    print("If neither one of you contributes, no one gains or loses any coins")
+    print("You will play for a random amount of rounds, so you won't know when the last round will take place.")
+ 
+def options() -> None:
+    print("If you wanna contribute, type, \"A\"")
+    print("If you wanna cheat, type, \"B\"")
+    print("If you wanna quit and see your points(coins), type, \"C\"")
+
+
+
+def decision() -> None:
+
+    choice: str = str(input(f"Okay {player}, time to make a choice. What will it be? "))
+    global points
+    if choice == "A":
+            ChoiceA()
+    else:
+        if choice == "B":
+            ChoiceB(points)
+
+        else:
+            if choice == "C":
+                quit()
+            else:
+                print("whoops you gotta choose one of the three")
+                decision()
+        
+def quit() -> None:
+    global i
+    i = test + 1
+    
+    
+    
+
+def ChoiceA() -> None:
+        global points
+        global points_opponent
+        points = points - 1
+        points_opponent = points_opponent + 3 
+        if Dave() == 0:
+          print (f"Dave cheated! No coins for you{SAD_FACE}")
+          print (f"Your coins: {points}" )
+          print (f"Dave's coins: {points_opponent}")
+        else:
+            points_opponent = points_opponent - 1
+            points= points + 3
+            print(f"Dave contributed! 2 coins for you{COWBOY}!")
+            print(f"Your coins: {points}" )
+            print(f"Dave's coins: {points_opponent}")
+
+   
+def ChoiceB(coins: int) -> int:
+    global points_opponent
+    global points
+    if Dave() == 0:
+            points_Dave = points_opponent
+            points = coins
+            print(f"Both of you cheated! No coins for anyone {SAD_FACE}")
+            print(f"Your coins: {points}" )
+            print(f"Dave's coins: {points_opponent}")
+    else:
+        points_opponent = points_opponent - 1
+        points = coins + 3
+        print("Dave contributed! Sucker!")
+        print(f"Your coins: {points}" )
+        print(f"Dave's coins: {points_opponent}")
+        points
+    return(coins)
+
+
+def Dave()-> int:
+    Dave_choice = randint(0,1)
+    if Dave_choice == 0:
+        Dave_result = 0
+    else:
+        Dave_result = 3
+    return Dave_result
+
+def loop() -> None:
+    global test
+    round = int(randint(4,8))
+    test = round
+    global i
+    i = 0
+    while i < round:
+        print (f"---Round {(i+1)}---")
+        decision()
+        i = i + 1
+   
+
+def main() -> None:
+    greet()
+    wants_instructions: str = input("Do you wanna hear the instructions? Type 'yes' or 'no': ")
+    if wants_instructions == 'yes':
+        instructions()
+    else:
+         print("Let's get to it")
+    print("")
+    options()
+    loop()
+    print ("That's game")
+    print(f"Thanks for playing, {player}, you ended the game with {points} coins! ")
+
+    
+
+    
+if __name__ == "__main__":
+    main()
+
+
